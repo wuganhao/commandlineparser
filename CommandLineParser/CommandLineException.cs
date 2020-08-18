c9614646086916dadabe19e4c555da7a08cfc3e8
Wu, Ganhao <wuganhao@hotmail.com>
2020-08-18 16:30:21 +0800

    Add first version

---
+++ CommandLineParser/CommandLineException.cs
@@ -0,0 +1,26 @@
+﻿/**********************************************************************************************************************
+ * Copyright WuGanhao. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * WuGanhao. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered
+ * into with WuGanhao.
+ *********************************************************************************************************************/
+
+using System;
+
+namespace WuGanhao.CommandLineParser {
+    /// <summary>
+    /// Command line exception
+    /// </summary>
+    public class CommandLineException: SystemException {
+
+        /// <summary>
+        /// Create new instance of CommandLineException
+        /// </summary>
+        /// <param name="message"></param>
+        public CommandLineException(string message): base(message) {
+        }
+    }
+}
---
+++ CommandLineParser/SubCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WuGanhao.CommandLineParser
+{
+    public abstract class SubCommand
+    {
+        public SubCommand() {
+        }
+
+        public abstract bool Run ();
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+
+    public class SubCommandAttribute: Attribute {
+        public SubCommandAttribute(Type cmdType, string command, string description) {
+            this.Type = cmdType;
+            this.Command = command;
+            this.Description = description;
+        }
+
+        public Type Type { get; set; }
+
+        public string Command { get; set; }
+
+        public string Description { get; set; }
+    }
+}
